473b6daf0581a685ca424a5efeb538c227d31377
Nikolay Yakimov <root@livid.pp.ru>
2017-01-30 20:37:17 +0300

    Fixes

--- clientaway.py
+++ clientaway.py
@@ -27,27 +27,24 @@ class clientaway(znc.Module):
 
         self.PutModule('\n'.join(output))
 
-    def SetAwayReasonCommand(self, sLine):
-        reason = ' '.join(sLine.s.split()[1:])
-
+    def SetAwayReasonCommand(self, reason):
         if reason:
             self.SetNV("reason", reason)
             self.PutModule("Away reason set to [{}]".format(reason))
 
         self.PutModule("Away message will be expanded to [{}]".format(self.GetAwayReason()))
 
-    def AutoAwayCommand(self, sLine):
-        self.SetNV("autoaway", ' '.join(sLine.s.split()[1:]))
+    def AutoAwayCommand(self, message):
+        self.SetNV("autoaway", message)
 
         if self.GetAutoAway():
             self.PutModule("Auto away when last client goes away or disconnects enabled.")
         else:
             self.PutModule("Auto away when last client goes away or disconnects disabled.")
 
-    def SetAwayCommand(self, sLine):
+    def SetAwayCommand(self, sHostname):
         clients = self.GetUser().GetAllClients()
 
-        sHostname = sLine.s.split()[1]
         count = 0
 
         for client in clients:
@@ -57,8 +54,8 @@ class clientaway(znc.Module):
 
         self.PutModule("{} clients have been set away".format(count))
 
-    def onModCommand(self, scmd):
-        toks = scmd.s.split()
+    def OnModCommand(self, scmd):
+        toks = scmd.split()
         cmd = toks[0].lower()
         args = toks[1:]
         if cmd == 'list':
@@ -84,59 +81,23 @@ class clientaway(znc.Module):
 
     def OnUserRaw(self, sLine):
         sCmd = sLine.s.split()[0]
+        sAwayReason = ' '.join(sLine.s.split()[1:])
 
-        if sCmd == "AWAY":
-            if (self.GetClient().IsAway()):
+        if sCmd.lower() == "away":
+            if not sAwayReason:
                 self.GetClient().SetAway(False)
-                self.GetClient().PutClient(":irc.znc.in 305 {} :You are no longer marked as being away".format(self.GetClient().GetNick()))
+                self.GetClient().PutClient(":irc.znc.in 305 {} :[Client] You are no longer marked as being away".format(self.GetClient().GetNick()))
 
                 if self.GetNetwork():
-                    pass #TODO
-                    # const vector<CChan*>& vChans = m_pNetwork->GetChans()
-                    # vector<CChan*>::const_iterator it
-                    #
-                    # for (it = vChans.begin(); it != vChans.end(); ++it) {
-                    #     // Skip channels which are detached or we don't use keepbuffer
-                    #     if (!(*it)->IsDetached() && (*it)->AutoClearChanBuffer()) {
-                    #         (*it)->ClearBuffer()
-                    #     }
-                    # }
-                    #
-                    # for (CQuery* pQuery : m_pNetwork->GetQueries()) {
-                    #     m_pNetwork->DelQuery(pQuery->GetName())
-                    # }
-                    #
-                    # if (GetAutoAway() && m_pNetwork->IsIRCAway()) {
-                    #     PutIRC("AWAY")
-                    # }
+                    if self.GetAutoAway() and self.GetNetwork().IsIRCAway():
+                         self.PutIRC("AWAY")
             else:
                 self.GetClient().SetAway(True)
-                self.GetClient().PutClient(":irc.znc.in 306 {} :You have been marked as being away".format(self.GetClient().GetNick()))
+                self.GetClient().PutClient(":irc.znc.in 306 {} :[Client] You have been marked as being away".format(self.GetClient().GetNick()))
 
                 if self.GetAutoAway() and self.GetNetwork() and not self.GetNetwork().IsIRCAway() and not self.GetNetwork().IsUserOnline():
-                    # Use the supplied reason if there was one
-                    sAwayReason = ' '.join(sLine.s.split()[1:])
+                    self.PutIRC("AWAY :{}".format(sAwayReason))
 
-                    if not sAwayReason:
-                        sAwayReason = self.GetAwayReason()
-
-                    PutIRC("AWAY :{}".format(sAwayReason))
-
-            return znc.HALTCORE
-
-        return znc.CONTINUE
-
-    def OnRaw(self, sLine):
-        # We do the same as ZNC would without the OnRaw hook,
-        # except we do not forward 305's or 306's to clients
-
-        sCmd = sLine.Token(1)
-
-        if sCmd == "305":
-            self.GetNetwork().SetIRCAway(False)
-            return znc.HALTCORE
-        elif sCmd == "306":
-            self.GetNetwork().SetIRCAway(True)
             return znc.HALTCORE
 
         return znc.CONTINUE
